e7deee2385b30c2e9e7267260ba31a74ab303214
Kishan Kumar Gupta <kishan.gupta@parabole.ai>
2022-12-06 19:53:29 +0530

    Revert "chan"
    This reverts commit 0498a85ae826dba4016da46b6d9c0ef7d979aec1.

--- linear_regression.py
+++ linear_regression.py
@@ -8,6 +8,6 @@ X = my_data[:, 0].reshape(-1, 1) # reshape(-1,1) tells python to convert the arr
                                 # “-1” tells python to figure out the rows by itself.
 ones = np.ones([X.shape[0], 1]) # create a array containing only ones
 X = np.concatenate([ones, X], 1) # cocatenate the ones to X matrix
-y = my_data[:, 1].reshape # create the y matrix
+y = my_data[:, 1].reshape(-1, 1) # create the y matrix
 
 print(plt.scatter(my_data[:, 0].reshape(-1, 1), y))
